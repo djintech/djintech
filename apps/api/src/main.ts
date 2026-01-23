bff378554525d8333f131d6b9c323134576e361f
Mary <mevdokimova@innodata.ru>
2026-01-23 17:48:25 -0500

    clear env

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,6 +3,7 @@ import { ApiModule } from './api.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(ApiModule);
+  app.setGlobalPrefix('api');
   await app.listen(process.env.port ?? 3000);
 }
 bootstrap();
