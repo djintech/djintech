9e4cd176a7b5720447095361746a5151fe026add
Mary <mevdokimova@innodata.ru>
2026-01-25 13:36:47 -0500

    add settings

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -4,7 +4,6 @@ import { ApiModule } from './api.module';
 async function bootstrap() {
   const app = await NestFactory.create(ApiModule);
   app.setGlobalPrefix('api');
-  await app.listen(process.env.port ?? 3000);
-  console.log('ENV NODE_ENV= ', process.env.NODE_ENV)
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
