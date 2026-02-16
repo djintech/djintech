ec53f5a573e0366a3fc1a6228ba1cf569f792be1
Mary <mevdokimova@innodata.ru>
2026-02-15 21:54:50 -0500

    change deployment.yaml, and config for test

--- libs/config/config-dynamic-module.ts
+++ libs/config/config-dynamic-module.ts
@@ -1,12 +1,15 @@
 import { ConfigModule } from "@nestjs/config";
-import { envFilePaths } from "./env-file-paths";
 import configuration, { ServiceName } from "./configuration";
+import { Environments } from "./base-core.config";
+import { getEnvFilePath } from "./env-file-paths";
 
 //console.log('Loading env files from:', envFilePaths);
 // must import this const in the head of your app.module.ts
 export const configModule = (serviceName: ServiceName) =>
   ConfigModule.forRoot({
   isGlobal: true, //isGlobal делает модуль глобальным (автоматически регистрируется в каждый модуле)  
-  envFilePath: envFilePaths,
+  ignoreEnvFile:
+      process.env.NODE_ENV !== Environments.DEVELOPMENT && process.env.NODE_ENV !== Environments.TESTING,
+  envFilePath: getEnvFilePath(process.env.NODE_ENV as Environments),//envFilePaths,
   load: [() => configuration(serviceName)],
 });
--- libs/config/env-file-paths.ts
+++ libs/config/env-file-paths.ts
@@ -1,15 +1,26 @@
-import { join } from 'path';
+import { Environments } from "./base-core.config";
 
-if (!process.env.NODE_ENV) {
-  throw new Error('NODE_ENV is required');
-}
+// if (!process.env.NODE_ENV) {
+//   throw new Error('NODE_ENV is required');
+// }
 
- export const envFilePaths = [
-    process.env.ENV_FILE_PATH?.trim() || '',
-    `.env.${process.env.NODE_ENV}.local`,
-    `.env.${process.env.NODE_ENV}`,
-    '.env.production',
+//  export const envFilePaths = [
+//     process.env.ENV_FILE_PATH?.trim() || '',
+//     `.env.${process.env.NODE_ENV}.local`,
+//     `.env.${process.env.NODE_ENV}`,
+//     '.env.production',
+//   ];
+
+export const getEnvFilePath = (env: Environments) => {
+  const defaultEnvFilePath = [
+    '.env.development.local',
+    '.env.development',
+    '.env',
   ];
 
-  
-  
+  if (env === Environments.TESTING) {
+    return [ '.env.test', ...defaultEnvFilePath];
+  }
+
+  return defaultEnvFilePath;
+};
