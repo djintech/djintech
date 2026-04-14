bf3d9d3ccf7e8469051f59e876d95a723c78c1d6
Mary <mevdokimova@innodata.ru>
2026-04-14 13:14:54 -0400

    edit post api update

--- apps/api/src/modules/posts/swagger/update-post.swagger.ts
+++ apps/api/src/modules/posts/swagger/update-post.swagger.ts
@@ -2,13 +2,13 @@
 import { applyDecorators } from '@nestjs/common';
 import { ApiBadRequestResponse, ApiBody, ApiForbiddenResponse, ApiNoContentResponse, ApiNotFoundResponse, ApiOperation, ApiSecurity, ApiUnauthorizedResponse } from '@nestjs/swagger';
 import { ErrorResponseDto } from '@src/core/error-dto/error-response.dto';
-import { PostInputDto } from '../api/input-dto/posts.input-dto';
+import { PostInputUpdateDto } from '../api/input-dto/posts.input-dto';
 
 export function ApiUpdatePostDocs() {
   return applyDecorators(
     ApiSecurity('JwtAuth'),
     ApiOperation({ summary: 'Update post' }),
-    ApiBody({ type: PostInputDto }),
+    ApiBody({ type: PostInputUpdateDto }),
     ApiNoContentResponse({ description: 'The post has been successfully updated' }),
     ApiNotFoundResponse({ description: 'Post not found', type: ErrorResponseDto }),
     ApiBadRequestResponse({ description: 'The inputModel has incorrect values', type: ErrorResponseDto }),
